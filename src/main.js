88cf92ea4831b054082438ac75d61ab1b71194d0
Alia Peterson <alia@pnwebs.com>
2020-12-31 01:06:06 -0700

    Add functionality to display cards from server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
+// query selectors
+
 const inputName = document.getElementById('input--name')
 const inputDiet = document.getElementById('input--diet')
 const inputFact = document.getElementById('input--fact')
+const inputButton = document.getElementById('input--button')
+const cardTemplate = document.getElementById('template--card')
+const cardDisplay = document.querySelector('.main--cards')
+
+// event handlers
+
+window.addEventListener('load', displayStoredCards)
+inputButton.addEventListener('click', createNewCard)
 
 const savedCards = {
   method: 'GET'
@@ -19,11 +29,26 @@ const newCard = {
   }
 }
 
-fetch('http://localhost:3001/api/v1/animals')
-  .then(response => response.json())
-  .then(animal => console.log(animal))
+// functions
 
-fetch('http://localhost:3001/api/v1/animals', newCard)
+function displayStoredCards() {
+  fetch('http://localhost:3001/api/v1/animals')
+  .then(response => response.json())
+  .then(animals => {
+    animals.forEach(animal => {
+      const card = cardTemplate.content.cloneNode(true)
+      card.querySelector('h3.card--name').innerText = animal.name
+      card.querySelector('p.card--diet').innerText = animal.diet
+      card.querySelector('p.card--fact').innerText = animal.fun_fact
+
+      cardDisplay.appendChild(card)
+    })
+  })
+}
+
+function createNewCard() {
+  fetch('http://localhost:3001/api/v1/animals', newCard)
   .then(response => response.json())
   .then(animal => console.log(animal))
-  .catch(error => console.log(error));
+  .catch(error => console.log(error))
+}
